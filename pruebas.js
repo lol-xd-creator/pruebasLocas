45efd60833a7f74344ecdb7c0b275db414d35b97
benja <benjag261@gmail.com>
2026-08-16 11:15:38 -0300

    se empezo a probar tablas y objetos

---
+++ pruebas.js
@@ -0,0 +1,28 @@
+const lista = [];
+lista.push(4);
+lista.push("ashee");
+lista.push(777);
+
+
+for (const item of lista) {
+    console.log(item);
+}
+
+console.table(lista);
+
+const persona = {
+    nombre: "pepe",
+    edad: 777,
+    saludar() {
+        return `hola soy ${this.nombre}`;
+    },
+    mostrarTablaDeInformacion() {
+        liste = []
+        for (const [clave, valor] in Object.values(persona)) {
+            liste.push(`${clave} = ${valor}`);
+        }
+        console.table(liste);
+    }
+}
+
+persona.mostrarTablaDeInformacion();
